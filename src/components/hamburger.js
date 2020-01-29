01de032fdd2670ba7045b88b950ebade86f87735
Shane Fisher <shane.fisher@warrendouglas.com>
2020-01-29 12:23:35 -0600

    adds npm

--- src/components/hamburger.js
+++ src/components/hamburger.js
@@ -8,9 +8,9 @@ import hamburgerIcon from "../components/icons/hamburger.png"
 const hamburger = () => {
   const [open, setOpen] = useState(false)
   const transition = useTransition(open, null, {
-    from: { opacity: `0` },
-    enter: { opacity: `1` },
-    leave: { opacity: `0` },
+    from: { opacity: `0`, transform: "translate3d(100%,0,0)" },
+    enter: { opacity: `1`, transform: "translate3d(0%,0,0)" },
+    leave: { opacity: `0`, transform: "translate3d(100%,0,0)" },
   })
 
   const topdiv = css`
